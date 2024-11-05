437bb7b5bf6899f0569d2ca01e62c99c183ca33a
Glendson <gztomazetto@gmail.com>
2024-11-05 18:11:54 +0000

    feat: added custom theme and Loading Component

--- App.tsx
+++ App.tsx
@@ -5,19 +5,21 @@ import {
   Roboto_700Bold,
   Roboto_400Regular,
 } from "@expo-google-fonts/roboto";
+import { config } from "./config/gluestack-ui.config";
+import { Loading } from "./src/components/Loading";
 
 export default function App() {
   const [fontsLoaded] = useFonts({ Roboto_400Regular, Roboto_700Bold });
 
   return (
-    <GluestackUIProvider>
+    <GluestackUIProvider config={config}>
       <View>
         <StatusBar
           barStyle="light-content"
           backgroundColor="transparent"
           translucent
         />
-        {fontsLoaded ? <Text>Hello World!</Text> : <View></View>}
+        {fontsLoaded ? <Text>Hello World!</Text> : <Loading />}
       </View>
     </GluestackUIProvider>
   );
