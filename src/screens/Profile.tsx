9c825a3a99f03ec6bcb02a60daab9e0bf1daa56c
Glendson <gztomazetto@gmail.com>
2024-11-14 12:56:25 +0000

    feat: added manager photo on profile and custom toast insert on app

--- src/screens/Profile.tsx
+++ src/screens/Profile.tsx
@@ -1,23 +1,66 @@
-import { Center, Heading, Text, VStack } from "@gluestack-ui/themed";
+import { Center, Heading, Text, VStack, useToast } from "@gluestack-ui/themed";
 import { ScreenHeader } from "../components/ScreenHeader";
 import { ScrollView, TouchableOpacity } from "react-native";
 import { UserPhoto } from "../components/UserPhoto";
 import { Input } from "../components/Input";
 import { Button } from "../components/Button";
+import { ToastMessage } from "../components/ToastMessage";
+
+import * as ImagePicker from "expo-image-picker";
+import * as FileSystem from "expo-file-system";
+import { useState } from "react";
 
 export function Profile() {
+  const [userPhoto, setUserPhoto] = useState("https://github.com/Glendson.png");
+  const toast = useToast();
+
+  async function handleUserPhotoSelect() {
+    try {
+      const photoSelected = await ImagePicker.launchImageLibraryAsync({
+        mediaTypes: ImagePicker.MediaTypeOptions.Images,
+        allowsEditing: true,
+        aspect: [4, 4],
+        quality: 1,
+      });
+
+      if (photoSelected.canceled) return;
+
+      const photoUri = photoSelected.assets[0].uri;
+
+      if (photoUri) {
+        const photoInfo = (await FileSystem.getInfoAsync(photoUri)) as {
+          size: number;
+        };
+
+        if (photoInfo.size && photoInfo.size / 1024 / 1024 > 5) {
+          return toast.show({
+            placement: "top",
+            render: ({ id }) => (
+              <ToastMessage
+                id={id}
+                action="error"
+                title="Imagem muito grande!"
+                description="A imagem não pode ser salva pois é muito grande (máximo 5MB)."
+                onClose={() => toast.close(id)}
+              />
+            ),
+          });
+        }
+        setUserPhoto(photoUri);
+      }
+    } catch (error) {
+      console.error("Error selecting photo", error);
+    }
+  }
+
   return (
     <VStack flex={1}>
       <ScreenHeader title="Perfil" />
 
       <ScrollView contentContainerStyle={{ paddingBottom: 36 }}>
         <Center mt={"$6"} px={"$10"}>
-          <UserPhoto
-            source={{ uri: "https://github.com/Glendson.png" }}
-            alt=""
-            size="xl"
-          />
-          <TouchableOpacity>
+          <UserPhoto source={{ uri: userPhoto }} alt="" size="xl" />
+          <TouchableOpacity onPress={handleUserPhotoSelect}>
             <Text
               color={"$green500"}
               fontFamily={"$heading"}
